d82235242b4653deec1ccfb9305c03365ab68369
Matt Waite <matt.waite@gmail.com>
2013-05-24 10:34:44 -0500

    Better, but not complete documentation of the things I have working. Following the Google R Style guide better. More work to ensue.

--- TweetAnalysis.R
+++ TweetAnalysis.R
@@ -1,42 +1,57 @@
-First, install some necessary packages:
+# First, install some necessary packages:
 
-> install.packages('tm')
-> install.packages('RSQLite')
+install.packages('tm')
+install.packages('RSQLite')
 
-Now let's import the libraries
+# Now let's import the libraries
 
-> library("RSQLite")
-> library("tm")
+library("RSQLite")
+library("tm")
 
-First, let's connect to our database. We do this by creating a variable called con, and populate it with the connection. Be sure to change the path to your database.
+# First, let's connect to our database. We do this by creating a variable called con, and populate it with the connection. Be sure to change the path to your database.
 
-> con <- dbConnect(drv="SQLite", dbname="/Users/mwaite/Dropbox/Documents/writing/Poynter/TweetsWithR/tweets.db")
+con <- dbConnect(drv="SQLite", dbname="/Users/mwaite/Dropbox/Documents/writing/Poynter/TweetsWithR/tweets.db")
 
-If you're skeptical, you can take a look at the tables. There's only one.
+# If you're skeptical, you can take a look at the tables. There's only one.
 
-> tables <- dbListTables(con)
+tables <- dbListTables(con)
 
-While databases like tables, R doesn't. It likes things as dataframes. So we need to make our table into a dataframe.
+# While databases like tables, R doesn't. It likes things as dataframes. So we need to make our table into a dataframe.
 
-> tweetframe <- dbReadTable(con, "tweets")
+tweetframe <- dbReadTable(con, "tweets")
 
-
+# The basic data unit of text analysis is the lexical corpus. It's a dataset of documents. We'll create it here.
  
-> tweetframe.corpus <- Corpus(VectorSource(tweetframe))
+tweetframe.corpus <- Corpus(VectorSource(tweetframe))
 
-> tweetframe.corpus <- tm_map(tweetframe.corpus, tolower)
+# To work with that corpus, we need to clean it up. This is pretty easy to follow, but we're first going to make everything lowercase.
 
-> tweetframe.corpus <- tm_map(tweetframe.corpus, removePunctuation)
+tweetframe.corpus <- tm_map(tweetframe.corpus, tolower)
 
-> tweetframe.corpus <- tm_map(tweetframe.corpus, removeWords, stopwords("english"))
+# Now let's remove punctuation so "term" and "term." are not two different things.
 
-> tweetframe.dtm <- TermDocumentMatrix(tweetframe.corpus)
+tweetframe.corpus <- tm_map(tweetframe.corpus, removePunctuation)
 
-> tweetframe.dtm
+# Now let's remove all stop words. Stop words are things like "a", "and", "the", "of" and the like. We just want high value words.
 
-> findFreqTerms(tweetframe.dtm, lowfreq=30)
+tweetframe.corpus <- tm_map(tweetframe.corpus, removeWords, stopwords("english"))
 
-> findAssocs(tweetframe.dtm, 'drone', 0.20)
+# Let's explore our dataset now, first by creating a document term matrix (dtm). It gives you a list of terms that appear in the corpus.
 
-> tweetframe.dtm2 <- removeSparseTerms(tweetframe.dtm, sparse=0.95)
+tweetframe.dtm <- TermDocumentMatrix(tweetframe.corpus)
 
+# Wanna see it?
+
+tweetframe.dtm
+
+# Now, *every* term is probably not useful. Some appear more than others. Here's how to show only terms that appear 30 or more times. The more words your corpus has, the higher this number will need to be.
+
+findFreqTerms(tweetframe.dtm, lowfreq=30)
+
+# Not exactly sure what this does and certainly can't explain it. So more research here plz.
+
+findAssocs(tweetframe.dtm, 'drone', 0.20)
+
+# Same. It removes sparse terms to make life easier and puts it into a new dataframe. But I'm not sure what the .95 means yet. So, more here plz.
+
+tweetframe.dtm2 <- removeSparseTerms(tweetframe.dtm, sparse=0.95)
